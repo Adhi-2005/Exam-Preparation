48b73b26352c64cd429d9ec1dd477d3b88cce73c
Adhi-2005 <adhilshafeer1@gmail.com>
2024-01-12 07:05:54 +0400

    strdup done

---
+++ 42 Exams/Exam Rank 02/Level 2/ft_strdup/ft_strdup.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	char	*dest;
+
+	i = 0;
+	while (src[i])
+	{
+		i++;
+	}
+	i++;
+	dest = (char*)malloc(sizeof(char) * i + 1);
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char	src[] = "Hey Adhil";
+
+	printf("%s\n", ft_strdup(src));
+}
